a3004761f032d6300b7a9a55343a3c51a829cbae
MartaYG <martayebragon@gmail.com>
2023-10-05 17:40:49 +0200

    Creation of the domain module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ android {
 dependencies {
 
     implementation(project(":model"))
+    implementation(project(":domain"))
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
