0adddd9abe250666fb5aa8d7d286d74e3d9fd168
MartaYG <martayebragon@gmail.com>
2023-10-09 11:52:54 +0200

    Implemented an interceptor to store authentication token

--- datasource/src/main/java/com/martayg/datasource/remote/service/interceptors/AuthInterceptor.kt
+++ datasource/src/main/java/com/martayg/datasource/remote/service/interceptors/AuthInterceptor.kt
@@ -1,11 +1,26 @@
 package com.martayg.datasource.remote.service.interceptors
 
+import com.martayg.datasource.features.login.interfaces.SharedPreferencesManager
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
 import javax.inject.Inject
 
-class AuthInterceptor @Inject constructor(): Interceptor {
+class AuthInterceptor @Inject constructor(
+    private val sharedPreferences: SharedPreferencesManager
+): Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        TODO("Not yet implemented")
+        val token = sharedPreferences.authToken
+        val request:Request = chain.request()
+
+        return if(token.isNotEmpty()){
+            val authorizedRequest = request
+                .newBuilder()
+                .addHeader("Authorization", "Bearer $token")
+                .build()
+            chain.proceed(authorizedRequest)
+        }else{
+            chain.proceed(request)
+        }
     }
 }
