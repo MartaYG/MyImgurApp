aaf86e07cd47872e6de3e0da4ea0c260f95fa0db
MartaYG <martayebragon@gmail.com>
2023-10-10 21:33:31 +0200

    Creation of data class for image

--- model/src/main/java/com/martayg/model/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package com.martayg.model
-
-class MyClass {
-}
---
+++ model/src/main/java/com/martayg/model/features/gallery/Image.kt
@@ -0,0 +1,7 @@
+package com.martayg.model.features.gallery
+
+data class Image(
+    val imageId: String,
+    val link: String,
+    val username: String
+)
