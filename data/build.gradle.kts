91bfc29f92400fdf3b1b6dccb75b3ab6eadad104
MartaYG <martayebragon@gmail.com>
2023-10-10 21:59:49 +0200

    Implementing room and paging in data build gradle

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -43,6 +43,17 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
 
+    //room
+    implementation("androidx.room:room-runtime:2.5.0")
+    annotationProcessor("androidx.room:room-compiler:2.5.0")
+    kapt("androidx.room:room-compiler:2.5.0")
+    implementation("androidx.room:room-common:2.5.0")
+    implementation("androidx.room:room-ktx:2.5.0")
+
+    //paging
+    implementation("androidx.paging:paging-runtime-ktx:3.2.1")
+    implementation("androidx.paging:paging-compose:1.0.0-alpha20")
+
     //Hilt
     implementation("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
